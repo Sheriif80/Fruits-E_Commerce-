2cf80120c821c4a1dd00dfa23d6e5004c4d4d7c8
Sherif80 <sherifashraf1054@gmail.com>
2026-02-09 09:49:58 +0200

    just first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
