1db15c8120787212a0c04c5d39f7d3823f9c2c63
Sherif80 <sherifashraf1054@gmail.com>
2026-02-14 04:18:31 +0200

    provided the sign in cubit

--- lib/features/auth/presentations/views/login_view.dart
+++ lib/features/auth/presentations/views/login_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_e_commerce_app/core/services/get_it_service.dart';
 import 'package:fruits_e_commerce_app/core/widgets/custom_app_bar.dart';
+import 'package:fruits_e_commerce_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_e_commerce_app/features/auth/presentations/cubits/signin_cubit/signin_cubit.dart';
 import 'package:fruits_e_commerce_app/features/auth/presentations/views/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
@@ -7,11 +11,14 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(
-        appBar: showCustomAppBar(context, title: 'تسجيل دخول'),
-        body: const LoginViewBody(),
+    return BlocProvider(
+      create: (context) => SigninCubit(getIt.get<AuthRepo>()),
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: Scaffold(
+          appBar: showCustomAppBar(context, title: 'تسجيل دخول'),
+          body: const LoginViewBody(),
+        ),
       ),
     );
   }
