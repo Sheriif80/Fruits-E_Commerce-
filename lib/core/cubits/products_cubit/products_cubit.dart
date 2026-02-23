28d35b189a862baa299753657091f9c8246466eb
Sherif80 <sherifashraf1054@gmail.com>
2026-02-23 14:22:58 +0200

    Add ProductsCubit and ProductsState classes for product management

---
+++ lib/core/cubits/products_cubit/products_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_e_commerce_app/core/entities/product_entity.dart';
+import 'package:fruits_e_commerce_app/core/repos/products_repo/products_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productsRepo) : super(ProductsInitial());
+
+  final ProductsRepo productsRepo;
+
+  Future<void> getAllProducts() async {
+    emit(ProductsLoading());
+    final result = await productsRepo.getAllProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(message: failure.message)),
+      (products) => emit(ProductsLoaded(products: products)),
+    );
+  }
+
+  Future<void> getBestSellerProducts() async {
+    emit(ProductsLoading());
+    final result = await productsRepo.getBestSellerProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(message: failure.message)),
+      (products) => emit(ProductsLoaded(products: products)),
+    );
+  }
+}
---
+++ lib/core/cubits/products_cubit/products_state.dart
@@ -0,0 +1,20 @@
+part of 'products_cubit.dart';
+
+@immutable
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+final class ProductsLoading extends ProductsState {}
+
+final class ProductsLoaded extends ProductsState {
+  final List<ProductEntity> products;
+
+  ProductsLoaded({required this.products});
+}
+
+final class ProductsFailure extends ProductsState {
+  final String message;
+
+  ProductsFailure({required this.message});
+}
