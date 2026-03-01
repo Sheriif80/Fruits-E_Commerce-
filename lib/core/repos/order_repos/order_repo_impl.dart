f2dcbba29c544fd4ada53a4281dcf492a855168f
Sherif80 <sherifashraf1054@gmail.com>
2026-03-02 01:34:55 +0200

    Created the order repo and order repo implemntation

---
+++ lib/core/repos/order_repos/order_repo_impl.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_e_commerce_app/core/errors/failures.dart';
+import 'package:fruits_e_commerce_app/core/repos/order_repos/order_repo.dart';
+import 'package:fruits_e_commerce_app/core/services/database_service.dart';
+import 'package:fruits_e_commerce_app/core/utils/app_end_points.dart';
+import 'package:fruits_e_commerce_app/features/checkout/data/models/order_model.dart';
+import 'package:fruits_e_commerce_app/features/checkout/domain/entities/order_entity.dart';
+
+class OrderRepoImpl implements OrderRepo {
+  final DatabaseService databaseService;
+
+  OrderRepoImpl({required this.databaseService});
+  @override
+  Future<Either<Failures, void>> addOrder({
+    required OrderEntity orderEntity,
+  }) async {
+    try {
+      await databaseService.addData(
+        path: AppEndPoints.addOrders,
+        data: OrderModel.fromEntity(orderEntity).toJson(),
+      );
+      return const Right(null);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
--- lib/core/utils/app_end_points.dart
+++ lib/core/utils/app_end_points.dart
@@ -3,4 +3,5 @@ class AppEndPoints {
   static const getUserData = 'users';
   static const isUserExist = 'users';
   static const getProducts = 'products';
+  static const addOrders = 'orders';
 }
