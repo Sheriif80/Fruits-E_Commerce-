a5bce5c3b33f0560fef97bdd1cbc073edb0372bd
Sherif80 <sherifashraf1054@gmail.com>
2026-03-02 17:29:51 +0200

    Added the process payment method from the package instructions and add the info needed (client ID , secrect key and trancastions using the paypal payment entity)

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:fruits_e_commerce_app/constants.dart';
+import 'package:fruits_e_commerce_app/core/utils/app_keys.dart';
 import 'package:fruits_e_commerce_app/core/widgets/custom_button.dart';
 import 'package:fruits_e_commerce_app/features/checkout/domain/entities/order_entity.dart';
+import 'package:fruits_e_commerce_app/features/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart';
 import 'package:fruits_e_commerce_app/features/checkout/presentation/cubits/add_order_cubit/add_order_cubit.dart';
 import 'package:fruits_e_commerce_app/features/checkout/presentation/views/widgets/checkout_steps.dart';
 import 'package:fruits_e_commerce_app/features/checkout/presentation/views/widgets/checkout_steps_page_view_builder.dart';
@@ -66,10 +69,14 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
                 }
               }
               if (currentPage == 2) {
-                await context.read<AddOrderCubit>().addOrder(
+                _processPayment(context);
+
+                ////////////////////////
+                /*  await context.read<AddOrderCubit>().addOrder(
                   orderEntity: context.read<OrderEntity>(),
                 );
                 return;
+                */
               }
 
               pageController.animateToPage(
@@ -81,6 +88,34 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
           ),
           const Gap(25),
         ],
+      ),
+    );
+  }
+
+  void _processPayment(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => PaypalCheckoutView(
+          sandboxMode: true,
+          clientId: kPayPalClientID,
+          secretKey: kPayPalSecretKey,
+          transactions: [
+            PaypalPaymentEntity.fromEntity(
+              context.read<OrderEntity>(),
+            ).toJson(),
+          ],
+          note: "Contact us for any questions on your order.",
+          onSuccess: (Map params) async {
+            print("onSuccess: $params");
+          },
+          onError: (error) {
+            print("onError: $error");
+            Navigator.pop(context);
+          },
+          onCancel: () {
+            print('cancelled:');
+          },
+        ),
       ),
     );
   }
