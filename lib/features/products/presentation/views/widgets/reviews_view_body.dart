db68c887dcd6b06c7313e2c90a0b1b766eddc563
Sherif80 <sherifashraf1054@gmail.com>
2026-03-07 23:46:02 +0200

    very simple design in reviews view to test sending a review to firestore (gonna redesign)

--- lib/features/products/presentation/views/widgets/reviews_view_body.dart
+++ lib/features/products/presentation/views/widgets/reviews_view_body.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_e_commerce_app/core/models/review_model.dart';
+import 'package:fruits_e_commerce_app/core/widgets/custom_button.dart';
+import 'package:fruits_e_commerce_app/core/widgets/custom_text_form_field.dart';
+import 'package:gap/gap.dart';
 
 class ReviewsViewBody extends StatelessWidget {
   const ReviewsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Gap(16),
+            CustomTextFormField(hintText: "اكتب تعليقك..."),
+            const Gap(16),
+            CustomTextFormField(
+              hintText: "اكتب تقيمك...",
+              keyboardType: .number,
+            ),
+            const Gap(16),
+            CustomTextFormField(hintText: "اكتب تعليقك..."),
+            const Gap(16),
+
+            CustomButton(text: "ارسال", onPressed: () {}),
+          ],
+        ),
+      ),
+    );
   }
 }
