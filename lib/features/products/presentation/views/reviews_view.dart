0c0b30bee1ca1fc991fc0bb8fb60f8af2d351074
Sherif80 <sherifashraf1054@gmail.com>
2026-03-08 00:14:37 +0200

    Triggred the add review cubit and working now (Stilll needs working, that was only a test)

--- lib/features/products/presentation/views/reviews_view.dart
+++ lib/features/products/presentation/views/reviews_view.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_e_commerce_app/core/entities/product_entity.dart';
+import 'package:fruits_e_commerce_app/core/services/get_it_service.dart';
 import 'package:fruits_e_commerce_app/core/widgets/build_app_bar.dart';
+import 'package:fruits_e_commerce_app/features/products/domain/repos/reviews_repo.dart';
+import 'package:fruits_e_commerce_app/features/products/presentation/cubits/add_review_cubit/add_review_cubit.dart';
 import 'package:fruits_e_commerce_app/features/products/presentation/views/widgets/reviews_view_body.dart';
 
 class ReviewsView extends StatelessWidget {
-  const ReviewsView({super.key});
+  const ReviewsView({super.key, required this.productEntity});
+  final ProductEntity productEntity;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(title: "المراجعات", isNotificationVisible: false),
-      body: ReviewsViewBody(),
+    return BlocProvider(
+      create: (context) => AddReviewCubit(getIt.get<ReviewsRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(title: "المراجعات", isNotificationVisible: false),
+        body: ReviewsViewBody(productEntity: productEntity),
+      ),
     );
   }
 }
--- lib/features/products/presentation/views/widgets/reviews_view_body.dart
+++ lib/features/products/presentation/views/widgets/reviews_view_body.dart
@@ -1,32 +1,95 @@
 import 'package:flutter/material.dart';
-import 'package:fruits_e_commerce_app/core/models/review_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_e_commerce_app/core/entities/product_entity.dart';
+import 'package:fruits_e_commerce_app/core/entities/review_entity.dart';
+import 'package:fruits_e_commerce_app/core/helpers/get_user_data.dart';
+import 'package:fruits_e_commerce_app/core/utils/app_colors.dart';
 import 'package:fruits_e_commerce_app/core/widgets/custom_button.dart';
 import 'package:fruits_e_commerce_app/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_e_commerce_app/features/products/presentation/cubits/add_review_cubit/add_review_cubit.dart';
 import 'package:gap/gap.dart';
 
-class ReviewsViewBody extends StatelessWidget {
-  const ReviewsViewBody({super.key});
+class ReviewsViewBody extends StatefulWidget {
+  const ReviewsViewBody({super.key, required this.productEntity});
+  final ProductEntity productEntity;
 
+  @override
+  State<ReviewsViewBody> createState() => _ReviewsViewBodyState();
+}
+
+class _ReviewsViewBodyState extends State<ReviewsViewBody> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String? description;
+  num? rating;
+  String? image;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const Gap(16),
-            CustomTextFormField(hintText: "اكتب تعليقك..."),
-            const Gap(16),
-            CustomTextFormField(
-              hintText: "اكتب تقيمك...",
-              keyboardType: .number,
-            ),
-            const Gap(16),
-            CustomTextFormField(hintText: "اكتب تعليقك..."),
-            const Gap(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const Gap(16),
+              CustomTextFormField(
+                hintText: "اكتب تعليقك...",
+                onChanged: (value) {
+                  description = value;
+                },
+              ),
+              const Gap(16),
+              CustomTextFormField(
+                hintText: "اكتب تقيمك...",
+                keyboardType: .number,
+                onChanged: (value) {
+                  rating = num.parse(value);
+                },
+              ),
+              const Gap(16),
+              CustomTextFormField(
+                hintText: "اكتب تعليقك...",
+                onChanged: (value) {
+                  image = value;
+                },
+              ),
+              const Gap(16),
 
-            CustomButton(text: "ارسال", onPressed: () {}),
-          ],
+              BlocBuilder<AddReviewCubit, AddReviewState>(
+                builder: (context, state) {
+                  if (state is AddReviewError) {
+                    return Text(state.message);
+                  }
+                  if (state is AddReviewLoading) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: AppColors.primaryColor,
+                      ),
+                    );
+                  }
+
+                  return CustomButton(
+                    text: "ارسال التقييم",
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AddReviewCubit>().addReview(
+                          productCode: widget.productEntity.code,
+                          review: ReviewEntity(
+                            name: getUserData().name!,
+                            description: description!,
+                            rating: rating!,
+                            image: image!,
+                            date: DateTime.now().toString().substring(0, 10),
+                          ),
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
