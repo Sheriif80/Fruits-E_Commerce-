3c620fa97b61623a8e30098eac1038c7ad0eeda8
Sherif80 <sherifashraf1054@gmail.com>
2026-03-07 16:48:37 +0200

    Added some simple animations to the splash screen

--- lib/features/splash/presentations/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentations/views/widgets/splash_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:fruits_e_commerce_app/core/utils/app_assets.dart';
 import 'package:svg_flutter/svg.dart';
 
@@ -12,10 +13,39 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: .end,
-          children: [SvgPicture.asset(Assets.imagesPlant)],
+          children: [
+            SvgPicture.asset(Assets.imagesPlant)
+                .animate()
+                .fadeIn(duration: 800.ms)
+                .slide(
+                  begin: const Offset(-0.5, -0.5),
+                  end: Offset.zero,
+                  duration: 800.ms,
+                  curve: Curves.easeOut,
+                ),
+          ],
         ),
-        SvgPicture.asset(Assets.imagesLogo),
-        SvgPicture.asset(Assets.imagesBottom, fit: BoxFit.fill),
+        SvgPicture.asset(Assets.imagesLogo)
+            .animate()
+            .fadeIn(duration: 800.ms)
+            .scale(
+              begin: const Offset(0.5, 0.5), // Starts at 50% size
+              end: const Offset(1.0, 1.0), // Ends at 100% size
+              duration: 800.ms,
+              curve: Curves.easeOutBack, // Gives it a little bounce at the end
+            ),
+        SvgPicture.asset(Assets.imagesBottom, fit: BoxFit.fill)
+            .animate(
+              // This makes the floating effect repeat indefinitely
+              onPlay: (controller) => controller.repeat(reverse: true),
+            )
+            .fadeIn(duration: 1000.ms)
+            .moveY(
+              begin: 0,
+              end: -15, // Floats up by 15 pixels
+              duration: 1500.ms,
+              curve: Curves.easeInOut,
+            ),
       ],
     );
   }
