2dd6dd3d4858cfc852c8b5d16741cf6dc0a2ca2d
Sherif80 <sherifashraf1054@gmail.com>
2026-03-09 23:51:49 +0200

    Changed the user model and entity to be dealing with fcmToken

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -2,22 +2,39 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fruits_e_commerce_app/features/auth/domain/entities/user_entity.dart';
 
 class UserModel extends UserEntity {
-  UserModel({required super.email, required super.name, required super.userId});
+  UserModel({
+    required super.email,
+    required super.name,
+    required super.userId,
+    super.fcmToken,
+  });
 
-  factory UserModel.fromFirebaseUser(User user) => UserModel(
-    email: user.email ?? '',
-    name: user.displayName ?? '',
-    userId: user.uid,
-  );
+  factory UserModel.fromFirebaseUser(User user, {String? fcmToken}) =>
+      UserModel(
+        email: user.email ?? '',
+        name: user.displayName ?? '',
+        userId: user.uid,
+        fcmToken: fcmToken,
+      );
 
   factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
     email: json['email'] ?? '',
     name: json['name'] ?? '',
     userId: json['userId'] ?? '',
+    fcmToken: json['fcmToken'] ?? '',
   );
 
-  factory UserModel.fromEntity(UserEntity user) =>
-      UserModel(email: user.email, name: user.name, userId: user.userId);
+  factory UserModel.fromEntity(UserEntity user) => UserModel(
+    email: user.email,
+    name: user.name,
+    userId: user.userId,
+    fcmToken: user.fcmToken,
+  );
 
-  toMap() => {'name': name, 'email': email, 'userId': userId};
+  toMap() => {
+    'name': name,
+    'email': email,
+    'userId': userId,
+    'fcmToken': fcmToken,
+  };
 }
--- lib/features/auth/domain/entities/user_entity.dart
+++ lib/features/auth/domain/entities/user_entity.dart
@@ -2,10 +2,12 @@ class UserEntity {
   final String? name;
   final String? email;
   final String? userId;
+  final String? fcmToken;
 
   const UserEntity({
     required this.name,
     required this.email,
     required this.userId,
+    this.fcmToken,
   });
 }
